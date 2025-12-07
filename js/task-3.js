5ff901ac68680c058a21189bb863c6a51773922c
Vlad.Harkusha <vlad.garkusha1999@gmail.com>
2025-12-07 12:26:51 +0200

    end homework

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function filterArray(numbers, value) {
+    const array = [];
+    for (const arr of numbers) {
+        if (arr > value){
+            array.push(arr);
+        }
+    }
+    return array;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
